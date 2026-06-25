c7ed1c29b96ce0f0137095b803c56eab99c4982d
mayur2210-git <mayur.makwana2210@outlook.com>
2026-06-25 09:26:46 +0530

    Add firstday assigment file

---
+++ day01assignment.js
@@ -0,0 +1,38 @@
+/* var firstname
+console.log(typeof firstname);
+
+var companyname = "Testleaf";
+console.log(typeof companyname);
+
+var  mobileNumber =123244455
+console.log(mobileNumber);
+
+var isAutomation = true
+console.log(typeof isAutomation)
+
+var hasPlaywright
+console.log(typeof hasPlaywright); */
+
+
+let firstname = "Mayur"
+let companyName  ="TestLeaf"
+let mobileNumber = 123456789
+let isAutomation  =false
+let hasPlaywright 
+
+
+console.log(firstname);
+console.log(typeof firstname);
+console.log(companyName);
+console.log(typeof companyName);
+console.log(mobileNumber)
+console.log(typeof mobileNumber);
+console.log(isAutomation);
+console.log(typeof isAutomation);
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
+
+
